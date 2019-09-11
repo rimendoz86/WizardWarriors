4b8805867a7771f048aacebfe6bb93237243985a
Ricardo <rimendoz86@gmail.com>
2019-09-10 20:34:14 -0700

    SampleCPPProject

---
+++ SampleCPPProject/SampleCPPProject/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Rex
+ *
+ * Created on September 10, 2019, 6:01 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
