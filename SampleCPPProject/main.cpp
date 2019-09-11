27a2ebf02554205b9a90da907c38431c05dc9a4e
Ricardo <rimendoz86@gmail.com>
2019-09-10 20:48:46 -0700

    Change to main.cpp

--- SampleCPPProject/main.cpp
+++ SampleCPPProject/main.cpp
@@ -12,6 +12,7 @@
  */
 
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
@@ -19,7 +20,7 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-
+    cout << "Hello World";
     return 0;
 }
 
