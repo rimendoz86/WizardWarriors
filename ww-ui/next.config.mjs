ab9497276f74a3d469e9590c66941fd792de9778
sonastea <sonastea@gmail.com>
2024-11-25 21:37:24 -0800

    feat: eslint v9 & prettier

--- ww-ui/next.config.mjs
+++ ww-ui/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: process.env.NODE_ENV === "production" ? true : false,
-  swcMinify: true,
   output: "standalone",
+  distDir: "dist",
 };
 
-module.exports = nextConfig;
+export default nextConfig;
